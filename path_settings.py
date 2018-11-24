62137e96542a8f11bd2aada4f71cfe9e72bd64ff
ulie <ulrike.schnaithmann@gmail.com>
2018-11-24 23:46:13 +0100

    added plots

--- path_settings.py
+++ path_settings.py
@@ -1,7 +1,6 @@
 """paths for reuse throughout project"""
 
 DATA_DIR_DICOM = "./data/dicoms/"
-CONTOUR_TYPE = "/i-contours"
 DATA_DIR_CONTOUR = "./data/contourfiles/"  
 LINKFILE = "./data/link.csv"
 DATA_DIR_IMAGES = "./data/mask_images/"
